eed57beafe8b7ac7ad6c0de8ee165a238110c318
lldap <lldaphrich@gmail.com>
2023-06-04 13:43:25 +0200

    I've done a lot man. made the design

--- app/src/main/java/com/example/loginscreen/CreateProfileActivity.java
+++ app/src/main/java/com/example/loginscreen/CreateProfileActivity.java
@@ -1,9 +1,13 @@
 package com.example.loginscreen;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,7 +29,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CreateProfileActivity extends AppCompatActivity {
-    private static final String URL = "https://lamp.ms.wits.ac.za/home/s2541383/hala.php";
+    //   private static final String URL = "https://lamp.ms.wits.ac.za/home/s2541383/hala.php";
+    private int index = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +40,59 @@ public class CreateProfileActivity extends AppCompatActivity {
         Button BackButton = findViewById(R.id.BackButton);
         Button FinishButton = findViewById(R.id.FinishButton);
         EditText user = findViewById(R.id.Username1);
-        String pass = "yeah";
+
+        String pass = "lldaphrich@";
         //take text from
-//        int [] avatars = {R.drawable.baseline_bedtime_24, R.drawable.baseline_snowmobile_24,
-//                R.drawable.baseline_umbrella_24, R.drawable.baseline_umbrella_24, R.drawable.baseline_umbrella_24,
-//                R.drawable.baseline_umbrella_24};
+        int[] avatars = {R.drawable.baseline_sports_esports_24, R.drawable.baseline_diamond_24,
+                R.drawable.face, R.drawable.baseline_self_improvement_24, R.drawable.moon,
+                R.drawable.pokemon};
+
+
+        CardView cardView1 = findViewById(R.id.esport);
+        CardView cardView2 = findViewById(R.id.esport1);
+        CardView cardView3 = findViewById(R.id.esport2);
+        CardView cardView4 = findViewById(R.id.esport3);
+        CardView cardView5 = findViewById(R.id.esport4);
+        CardView cardView6 = findViewById(R.id.esport5);
+
+        cardView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                index = 0;
+            }
+        });
+        cardView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                index = 1;
+            }
+        });
+        cardView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                index = 2;
+            }
+        });
+        cardView4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                index = 3;
+            }
+        });
+        cardView5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                index = 4;
+            }
+        });
+        cardView6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                index = 5;
+            }
+        });
+
+
         //go back to sign up
         BackButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,70 +101,22 @@ public class CreateProfileActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        //  VolleyRequestHelper volleyRequestHelper = new VolleyRequestHelper(CreateProfileActivity.this);
         FinishButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 String UserName = user.getText().toString().trim();
                 if (!UserName.isEmpty()) {
-                    add_user_info(UserName, pass);
+                    VolleyRequestHelper volleyRequestHelper = new VolleyRequestHelper(CreateProfileActivity.this);
+                    volleyRequestHelper.insertData(UserName, index);
+                    Intent intent = new Intent(CreateProfileActivity.this, LoginActivity.class);
+                    startActivity(intent);
                 } else {
                     Toast.makeText(CreateProfileActivity.this, "Invalid input", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-
-//        FinishButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(CreateProfileActivity.this, LoginActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-    }
-
-    private void add_user_info(String UserName, String pass) {
-
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject jsonObject = new JSONObject(response);
-                            String status = jsonObject.optString("status");
-
-                            if (status.equals("success")) {
-                                Toast.makeText(CreateProfileActivity.this, "Data Submitted Successfully", Toast.LENGTH_SHORT).show();
-                                // Switch to login page or perform any other actions
-                                Intent intent = new Intent(CreateProfileActivity.this, LoginActivity.class);
-                                startActivity(intent);
-                            } else if (status.equals("error")) {
-                                String errorMessage = jsonObject.optString("message");
-                                Toast.makeText(CreateProfileActivity.this, "Error: " + errorMessage, Toast.LENGTH_SHORT).show();
-                            } else {
-                                Toast.makeText(CreateProfileActivity.this, "Unknown response", Toast.LENGTH_SHORT).show();
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                            Toast.makeText(CreateProfileActivity.this, "Error parsing response", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(CreateProfileActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
-                    }
-                }) {
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<>();
-                params.put("students_number", UserName);
-                params.put("passwordd", pass);
-                return params;
-            }
-        };
-
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(stringRequest);
     }
 }
+
--- app/src/main/java/com/example/loginscreen/LoginActivity.java
+++ app/src/main/java/com/example/loginscreen/LoginActivity.java
@@ -72,7 +72,7 @@ public class LoginActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                Intent intent = new Intent(LoginActivity.this, CreateProfileActivity.class);
                 startActivity(intent);
             }
         });
