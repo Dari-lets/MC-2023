6c80fccdd79033622c039b5de76ed4bebad6af0a
UniqLeo <phomelelomaloma@gmail.com>
2023-06-04 21:51:07 +0200

    Sign Up: storing the security question answers does not work yet. Login is done

--- app/src/main/java/com/example/loginscreen/LoginActivity.java
+++ app/src/main/java/com/example/loginscreen/LoginActivity.java
@@ -1,14 +1,18 @@
 package com.example.loginscreen;
 
+import static com.example.loginscreen.PasswordHasher.hashPassword;
+
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,9 +22,6 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.example.loginscreen.HomePage;
-import com.example.loginscreen.R;
-import com.example.loginscreen.SignupActivity;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -30,8 +31,8 @@ import java.util.Vector;
 
 public class LoginActivity extends AppCompatActivity {
 
-    EditText name;
-    EditText pass;
+    EditText username;
+    EditText password;
 
     EditText print;
     Button RegisterFromLogin;
@@ -39,6 +40,8 @@ public class LoginActivity extends AppCompatActivity {
     LinearLayout m;
     Button ForgotPasswordButton;
     TextView LsOnly;
+    TextView UsernameText;
+    TextView PasswordText;
 
     EditText Checker;
 
@@ -49,11 +52,13 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         m = new LinearLayout(this);
 
-        name = findViewById(R.id.username);
-        pass = findViewById(R.id.password);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
         RegisterFromLogin = findViewById(R.id.SignUpButton);
         login = findViewById(R.id.LoginButton);
         ForgotPasswordButton = findViewById(R.id.ForgotPasswordButton);
+        UsernameText = findViewById(R.id.UsernameText);
+        PasswordText = findViewById(R.id.PasswordText);
 
         LsOnly = new TextView(this);
         print = new EditText(this);
@@ -82,10 +87,28 @@ public class LoginActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                String studentNumber = name.getText().toString();
-                String password = pass.getText().toString();
+                String studentNumber = username.getText().toString();
+                String password1 = LoginActivity.this.password.getText().toString();
+                String HashedPassword = hashPassword(password1);
 
-                String url = "https://lamp.ms.wits.ac.za/home/s2541383/login.php";
+                boolean ValidInput = true;
+                //strings to hold the input
+                String name = username.getText().toString();
+                String pass = password.getText().toString();
+                if (name.isEmpty()) {
+                    ValidInput = false;
+                    UsernameText.setText("Required Field");
+                } else {
+                    UsernameText.setText("");
+                }
+                if (pass.isEmpty()) {
+                    ValidInput = false;
+                    PasswordText.setText("Required Field");
+                } else {
+                    PasswordText.setText("");
+                }
+                if (ValidInput) {
+                String url = "https://lamp.ms.wits.ac.za/home/s2549501/login.php";
 
                 StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                         new Response.Listener<String>() {
@@ -98,32 +121,39 @@ public class LoginActivity extends AppCompatActivity {
                                     m.setOrientation(m.VERTICAL);
 
                                     if (stuNums.size() == 0) {
-                                        LsOnly.setText("You don't exist bish!");
+                                        LsOnly.setText("Database empty, register now!");
                                         setContentView(m);
                                         m.addView(LsOnly);
                                     } else {
-                                        boolean loginSuccessful = false;
+
+                                        boolean found = false;
 
                                         for (int i = 0; i < stuNums.size(); i++) {
-                                            if (stuNums.get(i).equals(studentNumber) && stuPass.get(i).equals(password)) {
-                                                loginSuccessful = true;
+                                            String currentStuNum = stuNums.get(i);
+                                            String currentStuPass = stuPass.get(i);
+
+                                            if (currentStuNum.equals(studentNumber) && currentStuPass.equals(HashedPassword)) {
+                                                //Login successful, go to homepage
+                                                Intent intent = new Intent(LoginActivity.this, HomePage.class);
+                                                startActivity(intent);
+                                                finish();
+                                                found = true; // set flag to true
                                                 break;
+
                                             }
                                         }
 
-                                        if (loginSuccessful) {
-                                            //LsOnly.setText("Login Successful");
-                                            LoginSuccessful();
-                                        } else {
-                                            LsOnly.setText("Invalid credentials bish");
+                                        if (!found) {//invalid credentials
+                                            Toast toast = Toast.makeText(LoginActivity.this, "Invalid Credentials", Toast.LENGTH_SHORT);
+                                            toast.setGravity(Gravity.TOP, 0, 0);
+                                            toast.show();
                                         }
 
-                                        setContentView(m);
-                                        m.addView(LsOnly);
                                     }
                                 } catch (JSONException e) {
+                                    //this is also for us, for testing
                                     Log.e("JSONException", e.getMessage()); // Log the error
-                                    LsOnly.setText("Error occurred");
+                                    LsOnly.setText("Something bad has happened...");
                                     setContentView(m);
                                     m.addView(LsOnly);
                                 }
@@ -143,6 +173,7 @@ public class LoginActivity extends AppCompatActivity {
                 RequestQueue requestQueue = Volley.newRequestQueue(LoginActivity.this);
                 requestQueue.add(stringRequest);
             }
+        }
         });
 
     }
@@ -153,7 +184,7 @@ public class LoginActivity extends AppCompatActivity {
 
         for (int i = 0; i < q.length(); i++) {
             JSONObject job = q.getJSONObject(i);
-            String studentNumber = job.getString("student_number");
+            String studentNumber = job.getString("STU_NUM");
             vecStuNums.add(studentNumber);
         }
         return vecStuNums;
@@ -165,7 +196,7 @@ public class LoginActivity extends AppCompatActivity {
 
         for (int i = 0; i < q.length(); i++) {
             JSONObject job = q.getJSONObject(i);
-            String password = job.getString("passwordd");
+            String password = job.getString("PASSWORD");
             vecStuPass.add(password);
         }
         return vecStuPass;
